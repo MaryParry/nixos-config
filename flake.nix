d116d84cad069a3a517b9719cb4f26c9ea69e164
MaryParry <Maryam517@proton.me>
2026-04-30 20:47:36 +0400

    noctalia added

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
       
       
- #   noctalia = {
-#    inputs.nixpkgs.follows = "nixpkgs";
-  #    };
+   noctalia = {
+    url = "github:noctalia-dev/noctalia-shell";
+    inputs.nixpkgs.follows = "nixpkgs";          
+      };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -15,7 +16,7 @@
       specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
-   #       ./noctalia.nix
+          ./noctalia.nix
       ];
     };
   };
