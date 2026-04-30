7ab4553cab3074d492ec93551ee16d865fb2fdbc
MaryParry <Maryam517@proton.me>
2026-04-30 21:04:51 +0400

    noctalia added, configuring autostart

--- noctalia.nix
+++ noctalia.nix
@@ -1,7 +1,11 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
     inputs.noctalia.nixosModules.default
+  ];
+
+  environment.systemPackages = with pkgs; [
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
