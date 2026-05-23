861f6baa074a8eb1a66b94f92e798c826fc0a4ca
MaryParry <Maryam517@proton.me>
2026-05-24 02:50:12 +0400

    abt to break somethiiiing

--- home.nix
+++ home.nix
@@ -68,7 +68,8 @@
     # Music & entertainment
     steam
     qbittorrent
-  ];
+    mpv
+    ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
